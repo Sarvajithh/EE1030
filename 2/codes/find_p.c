6e383a43d646ba1a6b64799ab100ee950da70c2f
Sarvajithh <sarvajithguddety@gmail.com>
2024-09-19 11:22:50 +0530

    Delete 2 directory

--- 2/codes/find_p.c
+++
@@ -1,7 +0,0 @@
-#include <stdio.h>
-void find_p(int x1,int y1, int x2, int x3, int y3) {
-    float p = (float)(y3*(x2-x1)+y1*(x3-x2))/(x3-x1);
-    printf("The value of p for which the points are collinear is: p = %.2f\n", p);
-}
-
-
